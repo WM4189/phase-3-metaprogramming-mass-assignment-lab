23def1934844169f9abfa8c3964fe7b37c91c902
William Mosto <will.mosto@gmail.com>
2021-09-23 07:35:22 -0400

    commit tree 15961635fe1d8892a1e5f15799002fe077d4d364

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 class Person
-  # your code here
+  def initialize(att)
+    att.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+binding.pry
+0
+
+# ruby lib/person.rb
